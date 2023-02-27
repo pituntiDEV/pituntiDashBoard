43dfceb1f5f0d23451578822ff0ea1bd44ddef67
Juan Miguel Zelaya <miguelzelayadev@gmail.com>
2023-02-27 02:00:21 -0600

    Se agrego el boton de copy para copiar los datos de auth

--- src/components/UsersList/AuthData.jsx
+++ src/components/UsersList/AuthData.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
 export const AuthData = ({user}) => {
+    const  copySomething=async(text)=> {
+        try {
+          const toCopy = `
+              Email:${user.auth.email}\n,
+              Password:${user.auth.password}
+          `
+          await navigator.clipboard.writeText(toCopy);
+          console.log('Text or Page URL copied');
+        }
+        catch (err) {
+          console.error('Failed to copy: ', err);
+        }
+      }
+
+     
   return (
     <div>
         <div className="email"><span className='fw-bold m-2'>Email:</span>{user.auth.email}</div>
         <div className="password"><span className='fw-bold m-2'>Password:</span> {user.auth.password}</div>
+        <button className='btn btn-info' onClick={copySomething}>Copy</button>
     </div>
   )
 }
