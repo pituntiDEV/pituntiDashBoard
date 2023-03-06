a1bcd264287dc9798e938ef21515e195ea1c7e58
Juan Miguel Zelaya <miguelzelayadev@gmail.com>
2023-03-06 00:36:13 -0600

    Se Agrego como informacion el total de usuarios filtrados

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -1,66 +1,69 @@
-import React, { useContext,useEffect,useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { appContext } from '../../context/AppContext';
 import { UserCard } from './UserCard/UserCard'
-import { SearchInput} from "./SearchInput/SearchInput";
-import {UsersFilter} from './UsersFilter/UsersFilter';
+import { SearchInput } from "./SearchInput/SearchInput";
+import { UsersFilter } from './UsersFilter/UsersFilter';
 import { useUserFilter } from '../../hook/useUserFilter';
 
 import "./UsersList.scss";
 import { Spinner } from '../Spinner/Spinner';
 
-export const UsersList = ({ users,setNewUserState }) => {
+export const UsersList = ({ users, setNewUserState }) => {
   //Context
   const { socket, state: { openEditModal }, setOpenEditModal } = useContext(appContext);
-  
+
   //States
   const [usersFilter, setUsersFilter] = useState(users);
-  const [filterValue,setFilterValue]=useState({
+  const [filterValue, setFilterValue] = useState({
     nameOrEmail: "",
-    seller:"",
-    state:"",
-    byExpireDay:"",
-    server:""
+    seller: "",
+    state: "",
+    byExpireDay: "",
+    server: ""
   });
 
   //Custom Hooks
   const [filter] = useUserFilter(users);
-  
+
 
   //Effects
-  useEffect(()=>{
+  useEffect(() => {
     setUsersFilter(users);
-  },[users])
+  }, [users])
 
-  useEffect(()=>{
+  useEffect(() => {
     setUsersFilter(filter(filterValue));
-  },[filterValue])
+  }, [filterValue])
 
 
-   
+
   //Functions
   const search = (e) => {
-    setFilterValue({...filterValue,nameOrEmail: e.target.value})
+    setFilterValue({ ...filterValue, nameOrEmail: e.target.value })
   }
 
   const hanledChange = (e) => {
-    setFilterValue({...filterValue,[e.target.name]: e.target.value});
+    setFilterValue({ ...filterValue, [e.target.name]: e.target.value });
   }
 
 
   return (
     <div className="users">
 
-      
+
       <SearchInput onChange={search} />
-      <UsersFilter users={users} onChange={hanledChange}/>
-        <hr />
-       <div className="text-center">
-       {usersFilter.length}
-       </div>
+      <UsersFilter users={users} onChange={hanledChange} />
+      <hr />
+      <div className="text-center">
+       
+        <button type="button" class="btn btn-dark">
+        Total filtrados: <span class="badge text-bg-success"> {usersFilter.length}</span>
+        </button>
+      </div>
       <div className="users__container">
         {
           usersFilter.map((user) => {
-            return(<UserCard setNewUserState={setNewUserState} key={user._id} user={user}/>)
+            return (<UserCard setNewUserState={setNewUserState} key={user._id} user={user} />)
           })
         }
       </div>
