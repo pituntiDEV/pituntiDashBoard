dddeb4eb1144c386e5f681fbe748f56ec0e9924d
Juan Miguel Zelaya <miguelzelayadev@gmail.com>
2023-03-08 02:28:04 -0600

    Se agrego para buscar por palabras claves en google drive

--- src/pages/gdrive/Gdrive.jsx
+++ src/pages/gdrive/Gdrive.jsx
@@ -7,6 +7,7 @@ export const Gdrive = () => {
     // State 
     const [query,setQuery] = useState("");
     const [files,setFiles] = useState([]);
+    const [filesFilter,setFilesFilter] = useState([]);
 
     // Custom Hooks
     const [searchFiles,loading] = useFetchApi({
@@ -17,15 +18,32 @@ export const Gdrive = () => {
     }
     const search=()=>{
         setFiles([]);
+        setFilesFilter([])
         searchFiles({body:JSON.stringify({query})})
             .then(files=>{
                 setFiles(files);
+                setFilesFilter(files)
             })
+    }
+
+    const filer=(e)=>{
+        const value= e.target.value;
+        const filesFilters = files.filter(file=>{
+            if(value!=""){
+                return file.name.includes(value)
+            }
+            return file
+        })
+
+         setFilesFilter(filesFilters);
+          
     }
   return (
     <div className='Gdrive'>
         <Search placeholder="Escribe el nombre del archivo a buscar" onChange={onChange} onClick={search} className="gdrive__search"/>
-        <FilesList loadingFiles={loading} files={files}/>
+        <Search placeholder="Buscar palabras claves Ejem:1x1 s01e01 s1e2" onChange={filer}  className="gdrive__search"/>
+       
+        <FilesList loadingFiles={loading} files={filesFilter}/>
     </div>
   )
 }
