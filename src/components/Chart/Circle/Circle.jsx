336e504fdc7937ef8cd87ecad4d5396e2caa20f3
Juan Miguel Zelaya <miguelzelayadev@gmail.com>
2023-05-18 05:34:04 -0500

    Se agrego para que los resellers puedan ver el estado de usuarios del server

--- src/components/Chart/Circle/Circle.jsx
+++ src/components/Chart/Circle/Circle.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import "./Style.scss"
-export const Circle = ({title="title",porcentage="50",max=100}) => {
-    const porcentajeUso = (porcentage / max) * 100
+export const Circle = ({ title = "title", porcentage = "50", max = 100 }) => {
+  const porcentajeUso = (porcentage / max) * 100
   return (
     <div className='circleChart'>
 
-  <div className='porcentajes'  style={{
-    "--porcentaje": porcentage,
-    "--color": porcentajeUso <25?"#42b873":porcentajeUso < 75 ?"#e79421":"#c55c5c",
-    "--max":max
-  }} >
-    <svg width="150" height="150">
-      <circle r="65" cx="50%" cy="50%" pathLength={max} />
-      <circle r="65" cx="50%" cy="50%" pathLength={max} />
-    </svg>
-    <span>
-        
-        {Math.round(porcentajeUso,1)}%
-        <small>{title}</small>
-    </span>
-  </div>
+      <div className='porcentajes' style={{
+        "--porcentaje": porcentage,
+        "--color": porcentajeUso < 25 ? "#42b873" : porcentajeUso < 75 ? "#e79421" : "#c55c5c",
+        "--max": max
+      }} >
+        <svg width="150" height="150">
+          <circle r="65" cx="50%" cy="50%" pathLength={max} />
+          <circle r="65" cx="50%" cy="50%" pathLength={max} />
+        </svg>
+        <span>
+
+          {Math.round(porcentajeUso, 1)}%
+          <small>{title}</small>
+          <small className='tiny'>{`${porcentage}/${max}`}</small>
+        </span>
+      </div>
 
 
     </div>
--- src/pages/DashBoard/components/Statistics/Statistics.jsx
+++ src/pages/DashBoard/components/Statistics/Statistics.jsx
@@ -5,39 +5,39 @@ import useFetchApi from '../../../../hook/useFetchApi'
 import { ActiveUsers } from './ActiveUsers'
 import "./Style.scss";
 export const Statistics = () => {
-    const [servers,setServers] = useState([]);
-    const [getServersWithUsers,loading] = useFetchApi({
-        url:`/api/server/get/all?users=true`,
+    const [servers, setServers] = useState([]);
+    const [getServersWithUsers, loading] = useFetchApi({
+        url: `/api/server/get/all?users=true`,
         method: 'GET',
     })
 
-    useEffect(()=>{
+    useEffect(() => {
         getServersWithUsers()
-            .then(data=>{
-               setServers(data)
+            .then(data => {
+                setServers(data)
             })
-    },[])
-  return (
-    <div className='statistics'>
-      {servers.length>0 && <h2>Servers Statistics:</h2>}
-        <div className="circle__container">
-            {
-                servers.map(server=>{
-                    return (
-                        <div className='server__data' key={server._id}>
-                        <Circle porcentage={server?.users?.length} max={server.limit|| 100} title={server?.data?.name}/>
-                        
-                        </div>
-                    )
-                })
-            }
+    }, [])
+    return (
+        <div className='statistics'>
+            {servers.length > 0 && <h2>Servers Statistics:</h2>}
+            <div className="circle__container">
+                {
+                    servers.map(server => {
+                        return (
+                            <div className='server__data' key={server._id}>
+                                <Circle porcentage={server?.users} max={server.limit || 100} title={server?.data?.name} />
+
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
+            <ActiveUsers />
+
+            {/* <PlexChart/> */}
+
 
         </div>
-        <ActiveUsers/>
-
-        {/* <PlexChart/> */}
-
-        
-    </div>
-  )
+    )
 }
