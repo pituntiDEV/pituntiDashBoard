2a34b6b4103b5e1af47fad8aa1a1813ba15e14fe
Juan Miguel Zelaya <miguelzelayadev@gmail.com>
2023-06-08 23:48:42 -0500

    Se cambio para poder seleccionar diferentes servers para ver la media mas reciente para los usuarios finales

--- src/pages/Accounts/components/NewEmbyAccountForm/NewEmbyAccountForm.jsx
+++ src/pages/Accounts/components/NewEmbyAccountForm/NewEmbyAccountForm.jsx
@@ -3,64 +3,65 @@ import { useState } from 'react';
 import SWAlert from '../../../../components/SwAlert/SWAlert';
 import useFetchApi from '../../../../hook/useFetchApi';
 import "./NewEmbyAccountForm.scss";
-export const NewEmbyAccountForm = ({setOpenModal,setNewAccountState}) => {
+export const NewEmbyAccountForm = ({ setOpenModal, setNewAccountState }) => {
 
     //State
-    const [formData,setFormData] =useState({
-        ip:"",
-        port:"8096",
-        apiKey:"",
-        name:""
+    const [formData, setFormData] = useState({
+        ip: "",
+        port: "8096",
+        apiKey: "",
+        name: ""
     })
 
     //Custom Hooks
-    const [addAccount,loading] = useFetchApi({
-        url:`/api/emby/accounts`,
+    const [addAccount, loading] = useFetchApi({
+        url: `/api/emby/accounts`,
         method: "POST",
     })
 
-    const onChange=(e)=>{
-        setFormData({...formData,[e.target.name]:e.target.value});
+    const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     }
 
-    const submit=(e)=>{
+    const submit = (e) => {
         e.preventDefault();
-        addAccount({body:JSON.stringify(formData)})
-            .then(data=>{
+        addAccount({ body: JSON.stringify(formData) })
+            .then(data => {
                 SWAlert.alert({
-                    title:data.message || "Success"
+                    title: data.message || "Success"
                 })
                 setOpenModal(false)
-                setNewAccountState(s=>!s);
+                setNewAccountState(s => !s);
             })
-            .catch(error=>{
+            .catch(error => {
                 console.log(error);
-              
+
             })
 
     }
-  return (
-    <form onSubmit={submit}>
-        <div className="form__group">
-            <label htmlFor="ip">Nombre del servidor</label>
-            <input onChange={onChange} type="text" name="name" value={formData.name} required placeholder='Escriba el nombre del servidor' id="name" />
-        </div>
-        <div className="form__group">
-            <label htmlFor="ip">Direccion ip</label>
-            <input onChange={onChange} pattern="^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$" type="text" name="ip" value={formData.ip} required placeholder='Ejem:53.123.34.54' id="ip" />
-        </div>
+    return (
+        <form onSubmit={submit}>
+            <div className="form__group">
+                <label htmlFor="ip">Nombre del servidor</label>
+                <input onChange={onChange} type="text" name="name" value={formData.name} required placeholder='Escriba el nombre del servidor' id="name" />
+            </div>
+            <div className="form__group">
+                <label htmlFor="ip">Direccion ip</label>
+                {/* pattern="^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$" */}
+                <input onChange={onChange} type="text" name="ip" value={formData.ip} required placeholder='Ejem:http:53.123.34.54' id="ip" />
+            </div>
 
-        <div className="form__group">
-            <label htmlFor="port">Puerto emby</label>
-            <input onChange={onChange} type="number" required name="port" placeholder='Ingresa puerto del servidor de emby' value={formData.port}  id="port" />
-        </div>
+            <div className="form__group">
+                <label htmlFor="port">Puerto emby</label>
+                <input onChange={onChange} type="number" name="port" placeholder='Ingresa puerto del servidor de emby' value={formData.port} id="port" />
+            </div>
 
-        <div className="form__group">
-            <label htmlFor="claveAPI">Clave API</label>
-            <input onChange={onChange} type="text" value={formData.claveAPI} required name="apiKey" placeholder='Ingresa Clave API' id="claveAPI" />
-        </div>
+            <div className="form__group">
+                <label htmlFor="claveAPI">Clave API</label>
+                <input onChange={onChange} type="text" value={formData.claveAPI} required name="apiKey" placeholder='Ingresa Clave API' id="claveAPI" />
+            </div>
 
-        <button className="btn btn-primary">Agregar</button>
-    </form>
-  )
+            <button className="btn btn-primary">Agregar</button>
+        </form>
+    )
 }
